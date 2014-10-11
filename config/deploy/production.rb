64179077e977e416ed2d668455a8a63aca736178
Christophe Verbinnen <djpate@gmail.com>
2014-10-11 00:07:56 -0700

    requirejs rails...

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,9 +8,11 @@ set :repo_url, "git@github.com:djpate/documentation.codes.git"
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, roles: :app, except: { no_release: true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :restart do
+    on roles(:app) do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
+  after "deploy", "restart"
 end
 
-after "deploy:restart"
